3090e6b655870d6042d9fa866f01dfeaae85fd53
joshua <joshuadavidoni@yahoo.com>
2022-03-30 18:27:06 +0100

    new

--- controllers/fetchBlogs.js
+++ controllers/fetchBlogs.js
@@ -1,16 +1,5 @@
 const handleFetchBlogs = (req, res, db) => {
-    db.select('*').from('blogs')
-    .then(blogs => {      
-        res.json(blogs.map(blog => {
-            return {
-                body: blog.body,
-                title: blog.title,
-                author: blog.name,
-                id: blog.id
-            }
-        }))
-    })                              
-    .catch(err => res.status(400).json('An error occured'))
+    res.send('yoooooooooooo')
 }
 
 module.exports = {
